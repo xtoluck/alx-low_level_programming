b45f521d5a56457ba47275ca4a1f233334f22cf3
Lucky <xtoluck.com>
2023-09-07 15:38:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main  printing without printf and puts
+ * main -  printing without printf and puts
  *
  * Return: always(1)
  *
@@ -9,6 +9,6 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(1);
+return (1);
 
 }
