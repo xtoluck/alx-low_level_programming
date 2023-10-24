4449efc665c497e7374675f7a0bfae6b711684df
Lucky <xtoluck.com>
2023-10-24 16:41:36 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,37 +13,36 @@ size_t print_listint_safe(const listint_t *head);
  */
 size_t looped_listint_len(const listint_t *head)
 {
-const listint_t *tortoise, *t_hare;
+const listint_t *rabbit, *shar;
 size_t nodes = 1;
 if (head == NULL || head->next == NULL)
 return (0);
-tortoise = head->next;
-t_hare = (head->next)->next;
-while (t_hare)
+rabbit = head->next;
+shar = (head->next)->next;
+while (shar)
 {
-if (tortoise == t_hare)
+if (rabbit == shar)
 {
-tortoise = head;
-while (tortoise != t_hare)
+rabbit = head;
+while (rabbit != shar)
 {
 nodes++;
-tortoise = tortoise->next;
-t_hare = hare->next;
+rabbit = rabbit->next;
+shar = shar->next;
 }
-tortoise = tortoise->next;
-while (tortoise != t_hare)
+rabbit = rabbit->next;
+while (rabbit != shar)
 {
 nodes++;
-tortoise = tortoise->next;
+rabbit = rabbit->next;
 }
 return (nodes);
 }
-tortoise = tortoise->next;
-t_hare = (hare->next)->next;
+rabbit = rabbit->next;
+shar = (shar->next)->next;
 }
 return (0);
 }
-
 /**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
