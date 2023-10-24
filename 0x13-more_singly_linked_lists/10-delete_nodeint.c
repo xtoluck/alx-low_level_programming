62ab9202ca93231f0388066e7cd27c129f9fff4b
Lucky <xtoluck.com>
2023-10-24 16:25:16 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *temp = *head;
+listint_t *tmp = *head;
 listint_t *current = NULL;
 unsigned int i = 0;
 if (*head == NULL)
@@ -16,18 +16,18 @@ return (-1);
 if (index == 0)
 {
 *head = (*head)->next;
-free(temp);
+free(tmp);
 return (1);
 }
 while (i < index - 1)
 {
-if (!temp || !(temp->next))
+if (!tmp || !(temp->next))
 return (-1);
-temp = temp->next;
+tmp = temp->next;
 i++;
 }
-current = temp->next;
-temp->next = current->next;
+current = tmp->next;
+tmp->next = current->next;
 free(current);
 return (1);
 }
