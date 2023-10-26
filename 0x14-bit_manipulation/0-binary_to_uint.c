01480efac198105ccb0213c6703c19a84191dc80
Lucky <xtoluck.com>
2023-10-26 11:47:34 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,21 +2,20 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
+ * @q: string containing the binary number
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *q)
 {
 int i;
 unsigned int dec_val = 0;
-if (!b)
+if (!q)
 return (0);
-for (i = 0; b[i]; i++)
+for (i = 0; q[i]; i++)
 {
-if (b[i] < '0' || b[i] > '1')
+if (q[i] < '0' || q[i] > '1')
 return (0);
-dec_val = 2 * dec_val + (b[i] - '0');
+dec_val = 2 * dec_val + (q[i] - '0');
 }
 return (dec_val);
 }
