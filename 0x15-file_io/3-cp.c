a38912e85b6194fbce4d0c8444e7eeadde37ce47
Lucky <xtoluck.com>
2023-11-02 18:05:20 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,66 +2,65 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @fileFrom: fileFrom.
- * @fileTo: fileTo.
- * @argVec: arguments vector.
+ * error_file - checks if [the given] files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(int fileFrom, int fileTo, char *argVec[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (fileFrom == -1)
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argVec[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (fileTo == -1)
+	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argVec[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
-
 /**
- * main - checks the particular code for the given Holberton School students.
- * @arg_C: number of the given arguments.
- * @argVec: The given arguments vector.
+ * main - check the code for [the given] Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  * Return: Always 0.
  */
-int main(int arg_C, char *argVec[])
+int main(int argc, char *argv[])
 {
-	int fileFrom, fileTo, err_close;
-	ssize_t n_chars, n_w_r;
-	char b_u_f[1024];
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-	if (arg_C != 3)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp fileFrom fileTo");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	fileFrom = open(argVec[1], O_RDONLY);
-	fileTo = open(argVec[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(fileFrom, fileTo, argVec);
-	n_chars = 1024;
-	while (n_chars == 1024)
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		n_chars = read(fileFrom, b_u_f, 1024);
-		if (n_chars == -1)
-			error_file(-1, 0, argVec);
-		n_w_r = write(fileTo, b_u_f, n_chars);
-		if (n_w_r == -1)
-			error_file(0, -1, argVec);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
 	}
-	err_close = close(fileFrom);
+	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	err_close = close(fileTo);
+	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
